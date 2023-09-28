0a36f70a96a43014b9e707a4b37931dc12b0a874
carlos1234567890210294 <143907461+carlos1234567890210294@users.noreply.github.com>
2023-09-28 17:13:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
-mp.onButtonEvent(mp.MultiplayerButton.Up, ControllerButtonEvent.Pressed, function (player2) {
-	
+mp.onControllerEvent(ControllerEvent.Connected, function (player2) {
+    mp.setPlayerIndicatorsVisible(true)
+    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+    mp.setPlayerIndicatorsVisible(false)
 })
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 1 . . . . . . . . . 
+    . . . . . 1 f 1 . . . . . . . . 
+    . . . . 1 f f f 1 . . . . . . . 
+    . . . 1 1 1 1 1 1 1 . . . . . . 
+    . . . . . 1 f 1 . . . . . . . . 
+    . . . . . 1 f 1 . . . . . . . . 
+    . . . . . 1 1 1 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(img`
